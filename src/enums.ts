77401677231a3ab37b7c48df14f52d968309508b
RoderCode <Rodersnow@gmail.com>
2023-05-04 18:25:21 +0200

    enum

--- src/enums.ts
+++ src/enums.ts
@@ -3,5 +3,19 @@
 // it allow us to define a set of named constants
 // basically enums is a list of constant values
 
+const PENDING: number = 0;
+const SHIPPING: number = 1;
+const DELIVERED: number = 3;
 
+enum OrderStatus {
+  PENDING,
+  SHIPPING,
+  DELIVERED,
+  RETURNED
+}
 
+const myStatus = OrderStatus.DELIVERED;
+
+function isDelivered(status:OrderStatus){
+    return status === OrderStatus.DELIVERED
+}
